51b14f1ba3a70574beffa8451be17c741be6b134
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2024-01-16 07:16:23 +0530

    chore: cleanup and add data

---
+++ src/data/index.ts
@@ -0,0 +1,83 @@
+type dataType = {
+  id: string;
+  name: string;
+  image: string;
+  email: string;
+  isHighlighted: boolean;
+};
+
+const DATA = [
+  {
+    id: "0",
+    name: "Anita Bhatia",
+    image: "https://picsum.photos/seed/00/200",
+    email: "anitabhatia@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "1",
+    name: "Bhanu Pratap",
+    image: "https://picsum.photos/seed/1/200",
+    email: "bhanup@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "2",
+    name: "Chandan Kumar",
+    image: "https://picsum.photos/seed/2/200",
+    email: "chandank@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "3",
+    name: "Dhruv Sharma",
+    image: "https://picsum.photos/seed/3/200",
+    email: "dhruvs@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "4",
+    name: "Eshaan Bansal",
+    image: "https://picsum.photos/seed/4/200",
+    email: "eshaanb@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "5",
+    name: "Fahad Khan",
+    image: "https://picsum.photos/seed/5/200",
+    email: "fahadk@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "6",
+    name: "Gaurav Sharma",
+    image: "https://picsum.photos/seed/6/200",
+    email: "gauravs@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "7",
+    name: "Himanshu Sharma",
+    image: "https://picsum.photos/seed/7/200",
+    email: "himanshus@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "7",
+    name: "Ishaan Rastogi",
+    image: "https://picsum.photos/seed/8/200",
+    email: "ishaanr@example.com",
+    isHighlighted: false,
+  },
+  {
+    id: "9",
+    name: "Jatin Bansal",
+    image: "https://picsum.photos/seed/9/200",
+    email: "jatinb@example.com",
+    isHighlighted: false,
+  },
+];
+
+export default DATA;
+export type { dataType };
